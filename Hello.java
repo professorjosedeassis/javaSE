c2e1574b225bc7deabb28a7e89d0b76fb4e3f1e2
José de Assis <josedeassisfilho@gmail.com>
2019-06-05 19:50:21 -0300

    Adicionei o arquivo Hello.java

---
+++ Hello.java
@@ -0,0 +1,11 @@
+/**
+* Hello World Java
+* @author José de Assis
+*/
+
+public class Hello{
+	//método principal
+	public static void main(String[] args){
+		System.out.println("Hello World");
+	}
+}
